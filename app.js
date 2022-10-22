f7207a305961556f741899308972bce6e1e4f0c7
Maksym Voloshyn <maxvol200727@gmail.com>
2022-10-22 22:56:03 +0300

    Add hash

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 let color = document.querySelectorAll(".color")
 let copy = document.querySelectorAll(".copy")
-
 function getRandomColor() {
     return "#"+Math.floor(Math.random()*16777215).toString(16);
 }
 function changeColor() {
+    let colors=[]
     color.forEach(color=>{
         console.log();
         if (color.children[1].classList.contains('fa-lock')===true) {
-            console.log(132);
+            colors.push(color.children[0].textContent)
         }else{
         let random = getRandomColor()
         color.style.backgroundColor=random
         color.children[0].innerHTML=random
+        colors.push(random)
     }
     })
+    addToHash(colors) 
 }
 document.addEventListener('keydown', (event)=>{
     if (event.key=="ArrowRight") {
@@ -39,3 +41,19 @@ copy.forEach(copy=>{
         alert("Copied")
         })
 })
+function addToHash(color=[]) {
+    document.location.hash=color.map(color=>{return color.substring(1)}).join("-")
+}
+function GetFromHash() {
+    if (document.location.hash.length>1) {
+        document.querySelector(".press").style.display="none"
+       let col = document.location.hash.substring(1).split("-")
+        for (let index = 0; index < col.length; index++) {
+            color[index].style.backgroundColor="#"+col[index]
+            color[index].children[0].innerHTML="#"+col[index]
+        }
+    }else{
+        changeColor()
+    }
+}
+GetFromHash()
